f1b819a7cd963ce0f9bf66ea3fae55caf3a8051b
jnmarie00 <jennacox322@u.boisestate.edu>
2025-10-03 14:03:40 -0700

    AddedRollScript
    Wrote and added a DodgeRoll script
    added to the player

---
+++ BootCamp2/Assets/DodgeRoll.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeRoll : MonoBehaviour
+{
+    private Movement2D move;
+    bool roll = false;
+    public int cooldownTime = 3;
+    [SerializeField] float boost = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = GetComponent<Movement2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keycode.Mouse1))
+        {
+            Debug.Log("rightClick");
+            
+            if (!roll)
+            {
+                Debug.Log("roll was false");
+                roll = true;
+                Dodge();
+            }
+        }
+    }
+
+    void Dodge()
+    {
+        move.Dodger(boost);
+        StartCoroutine(CoolDown());
+    }
+
+    IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(.1f);
+
+        float Sd = boost - (boost * 2);
+
+        move.Dodger(sd);
+        Debug.Log("coroutine started");
+
+        yield return new WaitForSeconds(cooldownTime);
+    }
+}
